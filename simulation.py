81d4cec809c487f676c8cc5468c812f7eb9cff2d
coendejongnl <c.g.h.dejong@student.tudelft.nl>
2019-03-07 20:09:10 +0100

    added a scale back to dimensions function, which scales the energy and the time to the corrected unit, and fixed indentation error which gave errors

--- simulation.py
+++ simulation.py
@@ -15,6 +15,10 @@ if __name__ == "__main__":
     pos,vel,temperature_evolution,pot_energy[0], kin_energy[0]=redistributing_velocity(vel, pos, force,pot_energy[0], kin_energy[0])
     
     pot_energy, kin_energy = calculate_time_evolution(vel, pos, force, pot_energy, kin_energy)
+    
+    
+    
+    time,kin_energy,pot_energy=scaling_to_correct_dimensions(time,kin_energy,pot_energy)
     total_energy=calculate_total_energy(kin_energy,pot_energy)
     
 #    drift = drift_velocity(vel,Nt,dim,drift)
@@ -23,12 +27,12 @@ if __name__ == "__main__":
     
     plt.title('kinetic energy of all particles')
     
-    plt.plot(time*(m*sigma**2/epsilon)**.5,kin_energy,label='kinetic energy')
-    plt.plot(time*(m*sigma**2/epsilon)**.5,total_energy,label='total energy')
-    plt.plot(time*(m*sigma**2/epsilon)**.5,pot_energy,label='potential energy')
+    plt.plot(time,kin_energy,label='kinetic energy')
+    plt.plot(time,total_energy,label='total energy')
+    plt.plot(time,pot_energy,label='potential energy')
     
     plt.xlabel('time (s)')
-    plt.ylabel('energy in units of epsilon')
+    plt.ylabel('energy (joule)')
     plt.grid(b=None, which='major', axis='both')
     plt.legend(loc='best')
     
