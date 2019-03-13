2236d64389accd682c16e9e861c528fe2e598c05
Vincent van de Kerkhof <v.vandekerkhof@student.tudelft.nl>
2019-03-13 10:21:04 +0100

    added virial theorem and calculation for pressure

--- simulation.py
+++ simulation.py
@@ -8,15 +8,16 @@ from config import *
 
 if __name__ == "__main__":
 
-    vel, pos, pot_energy, kin_energy, drift_velocity = build_matrices()
-    vel, pos, force, pot_energy[0], kin_energy[0] = initial_state(vel, pos, pot_energy[0], kin_energy[0])
+    vel, pos, pot_energy, kin_energy, drift_velocity, vir = build_matrices()
+    vel, pos, force, pot_energy[0], kin_energy[0], vir = initial_state(vel, pos, pot_energy[0], kin_energy[0], vir)
 
-    pos,vel,temperature_evolution,pot_energy[0], kin_energy[0],drift_velocity=redistributing_velocity(vel, pos, force,pot_energy[0], kin_energy[0],drift_velocity)
+    #pos,vel,temperature_evolution,pot_energy[0], kin_energy[0],drift_velocity=redistributing_velocity(vel, pos, force,pot_energy[0], kin_energy[0],drift_velocity)
     
-    pot_energy, kin_energy , drift_velocity= calculate_time_evolution(vel, pos, force, pot_energy, kin_energy,drift_velocity)
-    
+    pot_energy, kin_energy , drift_velocity, vir = calculate_time_evolution(vel, pos, force, pot_energy, kin_energy,drift_velocity,vir)
+
     time,kin_energy,pot_energy=scaling_to_correct_dimensions(time,kin_energy,pot_energy)
     total_energy=calculate_total_energy(kin_energy,pot_energy)
+    p = calculate_pressure(vir)
     
 #    drift = drift_velocity(vel,Nt,dim,drift)
 
@@ -42,6 +43,12 @@ if __name__ == "__main__":
     plt.xlabel('time (s)')
     plt.ylabel('velocity(m/s)')
     plt.grid(b=None, which='major', axis='both')
+    
+    plt.figure()
+    
+    plt.plot(time,p)
+    plt.xlabel('time (s)')
+    plt.ylabel('pressure')
         
     plt.show()
        
