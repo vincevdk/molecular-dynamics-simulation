0d3062187da965ddfba8a94dd35a1432a5af93c9
peter-janderks@hotmail.com <peter-jan.derks@student.auc.nl>
2019-03-16 13:10:08 +0100

    added dimensionless temperature en two time arrays for equilibration and simulation phase

--- src/config.py
+++ src/config.py
@@ -1,22 +1,24 @@
 import numpy as np
 
+# Chosen settings for simulation 
+dim = 3
+temperature = 100
+N_particle = 108
+L = 10  # size of the box in units sigma 
+timestep = 10**-14
+
 # Properties of Argon atoms and physical constants
 m = 6.6 * 10**-26
 sigma = 3.405*10**-10
 kb = 1.38*10**-23
 epsilon = 119.8*kb
 
-# Chosen settings for simulation
-dim = 3
-temperature = 500
-N_particle = 108
-L = 10  # size of the box in units sigma
-
-# create an array "time" containing times at which we are calculating
-timestep = 10**-14
-h = 0.001
-#h = timestep/(m*sigma**2/epsilon)**.5 # dimensionless_timestep
-
-Nt = 300 # number of timesteps  
-time = np.arange(0,Nt*h-0.0001,h)
+# Dimensionless variables
+temperature = temperature/119.8 # in units (epsilon/kb)
+h = timestep/(m*sigma**2/epsilon)**.5 # dimensionless timestep
+total_equilibration_time = 10 # in units of [(m * sigma^2 / epsilon)^0.5]
+total_simulation_time = 20 # in units of [(m * sigma^2 / epsilon)^0.5]   
+equilibration_time = np.arange(0, total_equilibration_time,h)
+simulation_time = np.arange(total_equilibration_time, 
+                            total_equilibration_time+total_simulation_time,h)
 
