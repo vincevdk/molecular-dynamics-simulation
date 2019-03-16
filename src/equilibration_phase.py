7d427a72d5b3b2561ee35f2c8dbf7c2509586359
peter-janderks@hotmail.com <peter-jan.derks@student.auc.nl>
2019-03-16 13:34:49 +0100

    added files equilibration_phase.py and production_phase.py

---
+++ src/equilibration_phase.py
@@ -0,0 +1,50 @@
+import numpy as np
+import numpy.ma as ma
+
+from src.config import *
+from src.observables import *
+from src.functions import *
+
+def redistribute_velocity(vel,pos):
+    kinetic_energy = calculate_kinetic_energy(0, vel)
+    rescaling_factor = (N_particles - 1)*3/2*temperature/kinetic_energy
+    vel = vel*rescaling_factor
+    return(vel)
+
+def equilibrate(vel, pos):
+    """function which rescales the velocity according to the required
+    temperature. This is done by running the time evolution and checking if
+    the temperature is correct at the end afterwhich the scaling factor
+    scales it to the required temperature untill it converges
+
+    Parameters:
+    -----------
+    vel: array of size (N_particle, 3)
+       The velocity of N particles in 3 dimensions. The first index of the
+       array corresponds to a particle.
+    pos: array of size (N_particle, 3)
+       The positon of N particles in 3 dimensions. The first index of the
+       array corresponds to a particle.
+
+    Results:
+    -------
+    pos:array of size (N_particle, 3)
+       The positon of N particles in 3 dimensions. The first index of the array
+       corresponds to a particle.
+    vel:array of size (N_particle, 3)
+       The velocity of N particles in 3 dimensions. The first index of the
+       array corresponds to a particle.
+    temperature_evolution: array of size (100)
+       testfunction which shows what the temperature was before rescaling
+       every loop. (not implemented in other parts of the code, except for
+       tests)
+   """
+    for v in range(total_equilibration_time):
+        vel = vel + h * force / 2
+        pos = (pos + h * vel) % L
+        min_dis, min_dir = calculate_minimal_distance_and_direction(pos)
+        force = calculate_force(min_dir, min_dis)
+        vel = vel + h * force / 2
+        if v%20 == 0:
+           vel = redistribute_velocity(vel,pos)
+    return(pos, vel)
---
+++ src/production_phase.py
@@ -0,0 +1,33 @@
+import numpy as np
+import numpy.ma as ma
+import matplotlib.pyplot as plt
+
+from scipy import spatial
+from src.config import *
+from src.observables import *
+from src.functions import *
+
+def calculate_time_evolution(vel, pos, potential_energy,
+                             kinetic_energy, drift_velocity, vir):
+
+    min_dis, min_dir = calculate_minimal_distance_and_direction(pos)           
+    force = calculate_force(min_dir, min_dis)                                  
+    potential_energy[0] = calculate_potential_energy(min_dis, 
+                                                     potential_energy[0])
+
+    kinetic_energy[0] = calculate_kinetic_energy(kinetic_energy[0], vel) 
+    vir[0] = virial_theorem(pos)
+
+    for v in range(1, Nt):
+        vel = vel + h * force / 2
+        pos = (pos + h * vel) % L
+        min_dis, min_dir = calculate_minimal_distance_and_direction(pos)
+        force = calculate_force(min_dir, min_dis)
+        vel = vel + h * force / 2
+        potential_energy[v] = calculate_potential_energy(min_dis, 
+                                                         potential_energy[v])
+        kinetic_energy[v] = calculate_kinetic_energy(kinetic_energy[v], vel)
+        drift_velocity[v, :] = np.sum(vel, axis=0)
+        vir[v] = virial_theorem(pos)
+
+    return(potential_energy, kinetic_energy, drift_velocity, vir)
