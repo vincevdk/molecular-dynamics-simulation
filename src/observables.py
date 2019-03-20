bd790579ede29ff8dff6a32bf4c941238791f42a
Vincent van de Kerkhof <v.vandekerkhof@student.tudelft.nl>
2019-03-20 09:20:08 +0100

    Added docstring

--- src/observables.py
+++ src/observables.py
@@ -3,7 +3,20 @@ import numpy.ma as ma
 from src.config import *
 
 def calculate_pressure(vir):
-    p = N_particle*kb*temperature/(L*L)-(1/3)*0.5*vir
+    """" Calculates the pressure devided by kb*T*rho
+    
+    parameters:
+    ------
+    vir: array of size simulationtime
+    
+    result:
+    ------
+    p: float
+        pressure of the system for each timestep
+    """
+    
+    p = 1 - kb*temperature/(3*N_particle)*0.5*vir
+
     return(p)
 
 
