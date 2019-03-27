506bd500daf4250cd9cc7a0d5135f948661c2c68
peter-janderks@hotmail.com <peter-jan.derks@student.auc.nl>
2019-03-27 21:38:41 +0100

    fixed compressibility density plot

--- compressibility_density_plot.py
+++ compressibility_density_plot.py
@@ -16,18 +16,22 @@ def compressibility_plot(density, compressibility):
     plt.figure()
     plt.plot(density,compressibility[0])
     plt.plot(density,compressibility[1])
-    plt.xlabel('$\rho$')
-    plt.ylabel('$\Beta P$')
-    plt.label((2.74, 1.35))
+    plt.xlabel(r'$\rho $')
+    plt.ylabel(r'$\beta P$')
+    plt.legend(('$T = 2.74$', '$T = 1.35$'))
     plt.show()
 
 if __name__ == "__main__":
     temp = [2.74,1.35]
     density = [0.1,0.2,0.3,0.4,0.5,0.6, 0.7, 0.8]
+    beta_P = np.zeros((len(temp),len(density)))
+
     compressibility = np.zeros((len(temp),len(density)))
     for j in range (len(temp)):
-        for i in range(len(compressibility)):
+        for i in range(len(density)):
             _, compressibility[j][i] = run_simulation(temp[j],density[i],False)
-
-    beta_rho = compressibility * density
-    compressibility_plot(density, compressibility)
+            print(compressibility[j][i],'compressibility')
+            print(compressibility[0] * density)
+    beta_P[0] = compressibility[0] * density
+    beta_P[1] = compressibility[1] * density
+    compressibility_plot(density, beta_P)
